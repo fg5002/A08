0f1ee06f88abd55093216aaa61d830ba9ccd3845
Attila <attila.ocsag@gmail.com>
2023-12-30 14:00:42 +0100

    fallback removed

--- svelte.config.js
+++ svelte.config.js
@@ -4,9 +4,7 @@ import adapter from '@sveltejs/adapter-static';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter({
-			fallback: '200.html'
-		})
+		adapter: adapter()
 	},
 
 	preprocess: [vitePreprocess({})]
