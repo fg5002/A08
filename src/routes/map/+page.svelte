3ba7e04bf15cd3b80cf2948f58ef50a9da41b24b
Attila <attila.ocsag@gmail.com>
2023-12-31 06:01:37 +0100

    leaflet min layers

--- src/routes/map/+page.svelte
+++ src/routes/map/+page.svelte
@@ -15,17 +15,7 @@
       url={'https://tile.openstreetmap.org/{z}/{x}/{y}.png'}
       options={{ minZoom: 7, maxZoom: 19, attribution: '&copy; OpenstreetMap'}}
       selected
-    />        
-    <TileLayer
-      name={'Google'}
-      url={'https://mt0.google.com/vt/lyrs=s&hl=en&x={x}&y={y}&z={z}&s=Ga'}
-      options={{ minZoom: 7, maxZoom: 21, attribution: '&copy; Google Maps'}}
-    />
-    <TileLayer
-      name={'ArcGIS'}
-      url={'https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}.jpg'}
-      options={{ minZoom: 7, maxZoom: 19, attribution: '&copy; ArcGIS'}}
-    />    
+    />          
     <TileLayer
       name={'Esri'}
       url={'https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}'}
