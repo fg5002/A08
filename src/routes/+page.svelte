285b7a2cc06cec466496b43751bb37ccd2cba78f
Attila <attila.ocsag@gmail.com>
2024-02-03 09:00:23 +0100

    overlays-content added

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,4 @@
 <div class="flex flex-col justify-center items-center h-screen">
   <h1 class="font-bold text-5xl">CT55B2</h1>
+  <input class="text-xl px-2 py-1 bg-yellow-200 border-2 border-yellow-600 rounded-lg mt-24" type="text">
 </div>
