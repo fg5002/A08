7eb9e711f607d4a8c49bfcfba5920f69794cc477
Attila <attila.ocsag@gmail.com>
2024-02-28 11:12:32 +0100

    geo modified

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,17 +1,40 @@
 <script>
 
-  let res = null;
-
+  let res = "";
+  let pon = 0;
+  let idx = 0;
+  let tx = 0;
+  
   const geoLocation = ()=>{
     navigator.geolocation.getCurrentPosition(position => {
       const cor = [ position.coords.latitude, position.coords.longitude ];
       cor.map(s=>parseFloat(s).toFixed(6));
-      res = `${res}\n${cor[0]},${cor[1]}; ${position.coords.accuracy.toFixed(2)}`;
+      pon = parseFloat(position.coords.accuracy.toFixed(2));
+      switch (true) {
+        case pon < 5:
+          tx = "Excellent"
+          break;
+        case pon >5 && pon < 50:
+          tx = "GOD"
+          break;
+        case pon > 50:
+          tx = "BAD"
+          break;      
+        default:
+          break;
+      }
+      if(idx<5){
+        res += `${cor[0]},${cor[1]}\n${tx} (${pon})\n`;
+      }else{
+        res = `${cor[0]},${cor[1]}\n${tx} (${pon})\n`;
+        idx = 0;
+      }
+      idx++
     }, error => {
-      res = `${res}\n${locationError(error)}`;
+      res = `${locationError(error)}\n`;
     }, {
       timeout: 15000,
-      maximumAge: 5000,
+      maximumAge: 1000,
       enableHighAccuracy: true
     })
   }
@@ -32,11 +55,13 @@
       
 </script>
 
-<div class="flex flex-col justify-start items-center h-screen mt-8">
-  <h1 class="font-bold text-5xl">CT55B2</h1>
+<div class="flex flex-col justify-start items-center h-[80wh] mt-8">
+  <h1 class="font-bold text-xl">CT55B2</h1>
 
-  <div class="flex flex-col mt-8 gap-4 items-start">
-    <textarea class="px-2 py-1 border-2 border-gray-500 rounded-md row-auto min-h-72 min-w-80">{res}</textarea>
+  <div class="flex flex-col mt-8 gap-4 items-start h-[70vh] w-[80vw] overflow-y-auto">
+    <textarea 
+      class="px-2 py-1 text-lg border-2 border-gray-500 rounded-md row-auto h-full w-full font-bold {tx == "BAD" ? 'text-red-500' : tx === "GOOD" ? 'text-orange-300' : 'text-green-400'}"
+    >{res}</textarea>
     <button class=" px-2 py-1 border-2 border-gray-500 rounded-md bg-yellow-200" on:click={geoLocation}>Find position</button>
   </div>
 </div>
